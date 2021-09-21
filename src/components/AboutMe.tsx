8fbb23a8560aa95334261d754871b80fc81d34cf
kdrbek06 <kdrbek@gmail.com>
2021-09-21 12:48:34 +0200

    Add some text to AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,11 @@
 
-
 export default function AboutMe () {
   return (
     <div className="aboutContainer">
-        <div>
-            
+        <div className="aboutText">
+            <p className="aboutTextTitle">Hi! I'm <strong>Kadir Bek</strong></p>
+            <p>Fullstack Web Developer with curiosity and passion in Becode</p>
+
         </div>
     </div>
   );
