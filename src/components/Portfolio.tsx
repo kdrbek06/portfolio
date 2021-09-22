da25a2e139493521000b5dccd49caf7290c42fa9
kdrbek06 <kdrbek@gmail.com>
2021-09-22 17:08:58 +0200

    Add image to portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,11 +1,40 @@
+import Project from "./Project";
+import project1 from "../img/project1.jpeg";
+
+const PROJECTS = [
+  {
+    image: project1,
+    title: "React project",
+    text: "This is my first project"
+  },
+  {
+    image: project1,
+    title: "React project",
+    text: "This is my first project"
+  },
+  {
+    image: project1,
+    title: "React project",
+    text: "This is my first project"
+  },
+  {
+    image: project1,
+    title: "React project",
+    text: "This is my first project"
+  },
+]
+
+
 export default function Portfolio () {
   return (
     <div>
+
       <div>
         <h3>Portfolio</h3><br />
       </div>
+
       <div className="card-group">
-          
+        {PROJECTS.map((project) => <Project {...project}/>)}
       </div>
       
     </div>
