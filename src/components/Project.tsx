b7b550d018ce0e8332874d9a135e7d0a5e8be9b5
kdrbek06 <kdrbek@gmail.com>
2021-09-22 22:35:41 +0200

    Add -card- clasname to project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,8 +7,8 @@ export interface IAppProps {
 
 export default function Project({ image, title, text }: IAppProps) {
     return (
-        <div className="">
-            <img src={image} width="250" height="250" className="card-img-top" alt="..."/>
+        <div className="card">
+            <img src={image} className="card-img-top" alt="..."/>
             <div className ="card-body">
             <h5 className ="card-title">{title}</h5>
             <p className ="card-text">{text}</p>
